b8fab5a5c2d164b0e37c2be42cdad84b9c6caf4b
cesare000 <70891510+cesare000@users.noreply.github.com>
2022-04-24 21:39:05 +0200

    Update Decision_maker_english_version.py

--- Decision_maker_english_version.py
+++ Decision_maker_english_version.py
@@ -1,14 +1,3 @@
-"""TO MY FUTURE EMPLOYER WHO IS VISITING MY GITHUB :) 
-   As you know I'm italian and this whole program is written 
-   in italian, I'm only 15 so it is probably bad written but it
-   is a very basic program, I'll explain it in the following lines:
-   have you ever been unncertain on what to choose? Well, in this 
-   program you can insert the number of things in which you wanna
-   choose, then you write them and this mini-software will choose for 
-   you. Have a good day and employ me please ;)."""
-
-
-
 import random
 
 LIST = []
